4a81b2a0fd834f093753e8f4166d71b8e7a9d3e9
MorseWayne <quzhihao98@outlook.com>
2018-06-05 18:30:44 +0800

    完成了world页面排序和时间处理

--- utils/timer.js
+++ utils/timer.js
@@ -1,8 +1,5 @@
 
-
-
 /**
- * 
  * @param {你的page对象.调用该函数的this} that 
  * @param {计时时间} remainedTime 
  */
@@ -23,7 +20,22 @@ function countDown(that, remainedTime) {
   }, 1000);
 }
 
+  //自定义函数，规定sort的排序规则为降序排列
+  function compareWithTime(Value1, Vaule2) {
+    var flag = parseFloat(Value1.created_time) - parseFloat(Vaule2.created_time);
+    if (flag < 0) {
+      return 1;
+    } else if (flag > 0) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+
+
 module.exports = {
   countDown: countDown,
+  compareWithTime:compareWithTime,
 }
 
