04d1880987e66f8ea1a32c13caa3e5b36db052c7
MorseWayne <quzhihao98@outlook.com>
2018-06-10 14:35:08 +0800

    noop

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -43,7 +43,6 @@ Page({
   },
 
   getAllAuthorized: function (e) {
-<<<<<<< HEAD
 
     console.log(e);
     //获取用户信息
@@ -68,31 +67,6 @@ Page({
       if (category == "Select") {
         wx.navigateTo({
           url: '../select/select?created_song_id=' + created_song_id + '&song_id=' + song_id,
-=======
-    if(this.data.firstIn){
-      this.setData({
-        firstIn: false
-      })
-      //获取用户信息
-      this.getUserInfo(e);
-      //获取录音权限
-      this.getRecorderAuthority();
-
-      //判断是否是通过分享链接转化来的用户，如果是直接跳到设了category相应界面
-      console.log("isShare:" + this.data.isShare);
-      if (this.data.isShare) {
-        var created_song_id = this.data.created_song_id;
-        var song_id = this.data.song_id;
-        var category = this.data.category;
-        if (category == "Select") {
-          wx.navigateTo({
-            url: '../select/select?created_song_id=' + created_song_id + '&song_id=' + song_id + '&isShare=' + 'true',
-          })
-        }
-      } else {
-        wx.switchTab({
-          url: '../post/post',
->>>>>>> 00773c5a7930a469e2b1e127a6a6b92954243654
         })
       }
     }
