3192dc0ad08a3c0c0dc4390bf2c83eff431276a5
yimuyisheng <439601575@qq.com>
2018-06-12 10:37:21 +0800

    select 添加 邀请好友button;sing dot.png 修改,progress propt_motto 提示修改;删除 无用的images/cover 和 avatar

--- pages/player/player.js
+++ pages/player/player.js
@@ -16,7 +16,7 @@ Page({
     currentLineNum:0,
     toLineNum: -1,
     playIcon: 'icon-pause',
-    cdCls: 'pause',
+    cdCls: 'play',
     dotsArray: new Array(2),
     currentDot: 0,
     isShare:false,
@@ -97,7 +97,7 @@ Page({
       currentLineNum: 0,
       toLineNum: -1,
       playIcon: 'icon-pause',
-      cdCls: 'pause',
+      cdCls: 'play',
     })
     this.getPlayInfoDataFromServer();
   },
@@ -112,10 +112,9 @@ Page({
     })
 
     this.setData({
-      currentTime_format:"0:00"
+      currentTime_format:"0:00",
     })
-    // wx.pauseBackgroundAudio();
-
+    
     // 监听音乐播放
     wx.onBackgroundAudioPlay(() => {
       that.setData({
